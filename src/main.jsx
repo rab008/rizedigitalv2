a785ffc666f10f337b9e2acbd7db58d8b79d75e6
Rab008 <11872509+rab008@users.noreply.github.com>
2025-06-17 12:56:28 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/rizedigitalv2">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
